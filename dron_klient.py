3f0010f55866402ebabeb9cdab9421286041cab9
Szymonnw <129615292+Szymonnw@users.noreply.github.com>
2026-02-26 22:50:47 +0100

    Update dron_klient.py

--- dron_klient.py
+++ dron_klient.py
@@ -10,8 +10,10 @@ for plik in os.listdir(sciezka_folderu):
     with open(os.path.join(sciezka_folderu, plik), 'rb') as zdjecie:
         paczka_pliki = {'zdjecie': zdjecie}
         paczka_tekst = {'tekst': f"taka jest nazwa {plik}"}
-        requests.post(URL, files=paczka_pliki, data=paczka_tekst)
+        odp = requests.post(URL, files=paczka_pliki, data=paczka_tekst)
+        print(f"Status: {odp.status_code} , {odp.text}")
     time.sleep(10)
+
 
 
 
